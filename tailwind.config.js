c3247569c400a9b8b23f595f1abb241d40115971
Aditya Rao <dev.adityarao@gmail.com>
2024-06-11 20:30:42 +0530

    Setup custom colors to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ["./index.html"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "hsl(0, 0%, 100%)",
+      accent: "hsl(277, 64%, 61%)",
+      clrMain: "hsl(233, 47%, 7%)",
+      clrCard: "hsl(244, 38%, 16%)",
+      statHeading: "hsla(0, 0%, 100%, 0.6)",
+      mainParagraph: "hsla(0, 0%, 100%, 0.75)",
+    },
     extend: {
       fontFamily: {
         inter: ["Inter", ...defaultTheme.fontFamily.sans],
